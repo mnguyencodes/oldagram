7040f11847319d1ad7ca68d129efc2a8c6b0eee4
Mark Nguyen <mark.t.nguyen@outlook.com>
2025-02-04 08:59:55 -0600

    Modularize JavaScript code. Add 'uuid'  and 'isLiked' data attributes to posts.

--- data.js
+++ data.js
@@ -1,3 +1,5 @@
+import {v4 as uuidv4} from "https://jspm.dev/uuid"
+
 export const posts = [
     {
         name: "Vincent van Gogh",
@@ -6,7 +8,9 @@ export const posts = [
         avatar: "images/avatar-vangogh.jpg",
         post: "images/post-vangogh.jpg",
         comment: "just took a few mushrooms lol",
-        likes: 21
+        likes: 21,
+        isLiked: false,
+        uuid: "19434e92-6050-4e2f-935b-16249a3bf450"
     },
     {
         name: "Gustave Courbet",
@@ -15,7 +19,9 @@ export const posts = [
         avatar: "images/avatar-courbet.jpg",
         post: "images/post-courbet.jpg",
         comment: "i'm feelin a bit stressed tbh",
-        likes: 4
+        likes: 4,
+        isLiked: false,
+        uuid: "2b8324b9-d638-48ea-9591-3d247f73edbc"
     },
         {
         name: "Joseph Ducreux",
@@ -24,6 +30,27 @@ export const posts = [
         avatar: "images/avatar-ducreux.jpg",
         post: "images/post-ducreux.jpg",
         comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 152
+        likes: 152,
+        isLiked: false,
+        uuid: "242eb136-9294-447f-91e7-bc5ca127b9f8"
     }
 ]
+
+
+// Generate UUIDs
+function generateUuid(number) {
+    const uuids = []
+    for (let i = 0; i < number; i++) {
+        uuids.push(uuidv4())
+    }
+    return uuids
+}
+
+// console.log(generateUuid(3))
+
+// Output
+// [
+//     "19434e92-6050-4e2f-935b-16249a3bf450",
+//     "2b8324b9-d638-48ea-9591-3d247f73edbc",
+//     "242eb136-9294-447f-91e7-bc5ca127b9f8"
+// ]
--- index.js
+++ index.js
@@ -1,34 +1,39 @@
 import {posts} from "/data.js"
 
-const postSection = document.querySelector("#post-section")
-
-let innerHtmlString = ""
 const heart = "/images/icon-heart.png"
 const comment = "/images/icon-comment.png"
 const dm = "/images/icon-dm.png"
 
-
-for (let post of posts) {
-    innerHtmlString +=
-    `<div class="post-header flex-post-header">
-        <div>
-            <img class="avatar-img" src="${post.avatar}" alt="Avatar icon ${post.name}.">
+function getPostHtml() {
+    let postHtml = ""
+    for (let post of posts) {
+        postHtml +=
+        `<div class="border-t post-header flex-post-header">
+            <div>
+                <img class="avatar-img" src="${post.avatar}" alt="Avatar icon ${post.name}.">
+            </div>
+            <div>
+                <h2 class="mb0">${post.name}</h2>
+                <p class="mt0">${post.location}</p>
+            </div>
         </div>
-        <div>
-            <h2 class="mb0">${post.name}</h2>
-            <p class="mt0">${post.location}</p>
-        </div>
-    </div>
-    <img class="portrait-img" src="${post.post}" alt="A portrait of ${post.name}.">
-
-    <div class="icons">
-        <div class="flex-icons">
-            <img class="icons-img" src="${heart}">
-            <img class="icons-img" src="${comment}">
-            <img class="icons-img" src="${dm}">
-        </div>
-        <h2 class="mt0 padding-l">${post.likes} likes</h2>
-        <p class="padding-l"><span class="username">${post.username}</span> ${post.comment}</p>
-    </div> `
+        <img class="portrait-img" src="${post.post}" alt="A portrait of ${post.name}.">
+    
+        <div class="icons">
+            <div class="flex-icons">
+                <img class="icons-img" src="${heart}">
+                <img class="icons-img" src="${comment}">
+                <img class="icons-img" src="${dm}">
+            </div>
+            <h2 class="mt0 padding-l">${post.likes} likes</h2>
+            <p class="padding-l"><span class="username">${post.username}</span> ${post.comment}</p>
+        </div> `
+    }
+    return postHtml
 }
-postSection.innerHTML = innerHtmlString
+
+function render() {
+    document.querySelector("#post-section").innerHTML = getPostHtml()
+}
+
+render()
